3f4979bbb1a5c77d7931f6a3c187dcdc0c4760ac
Anais A <alfonsi_anais@hotmail.fr>
2021-12-14 11:16:54 +0100

    fixed css issues, insert video in modal content

--- map.js
+++ map.js
@@ -1,15 +1,5 @@
 // France Map JS
 
-/*function showVideos(region) {
-    let name = region.dataset.name;
-    let regionVideos = document.querySelectorAll(`.one-video__container[data-name="${name}"]`);
-    for (let video of regionVideos) {
-        video.style.display === 'block' ? video.style.display = 'none' : video.style.display = 'block';
-        let h2 = document.createElement('h2');
-        h2.innerHTML = `${name}`;
-        video.appendChild(h2);
-    }
-}*/
 const allRegions = document.querySelectorAll('#france-map__container a');
 const allVideos = document.querySelectorAll('.one-video__container');
 
@@ -34,21 +24,22 @@ for (let region of allRegions) {
     });
 }
 
-function videoPoPup() {
-    const modal = document.querySelector('#videos-modal');
-    const closeBtn = modal.querySelector('.video-btn-close');
-    modal.style.display = 'block';
-    closeBtn.onclick = () => {
-        modal.style.display = "none";
-    }
-    window.onclick = (e) => {
-        if (e.target === modal) {
+for (let video of allVideos) {
+    video.addEventListener('click', () => {
+        const videoFrame = video.querySelector('iframe');
+        const modal = document.querySelector('#videos-modal');
+        const modalVideoFrame = modal.querySelector('.modal-content iframe');
+        const closeBtn = modal.querySelector('.video-btn-close');
+        modalVideoFrame.src = videoFrame.src;
+        modal.style.display = 'block';
+        closeBtn.onclick = () => {
             modal.style.display = "none";
         }
-    }
-}
-
-for (let video of allVideos) {
-    video.addEventListener('click', videoPoPup);
+        window.onclick = (e) => {
+            if (e.target === modal) {
+                modal.style.display = "none";
+            }
+        }
+    });
 }
 
